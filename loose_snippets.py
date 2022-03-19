a823acffd5eb7502aaeb960c9177090c4364908a
ncagle <ncagle@vols.utk.edu>
2022-03-18 19:08:54 -0500

    Early March New Snippets
    A few new snippets and several code blocks from existing scripts that might have useful logic.

--- loose_snippets.py
+++ loose_snippets.py
@@ -19,6 +19,7 @@ for dirpath, dirnames, filenames in walk:
 		write('{0} has {1} records'.format(filename, count))‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍
 
 
+
 ### Append feature classes using cursors
 fc = r"C:\input.gdb\newRoads"
 fc_out = r"C:\out.gdb\Roads"
@@ -38,6 +39,7 @@ with arcpy.da.SearchCursor(fc, field_list) as scursor:
 			icursor.insertRow(row)
 
 
+
 # Finds empty fields and NULL geometry.
 populated = lambda x: x is not None and str(x).strip() != ''
 fc_fields = ['foobar', 'SHAPE@']
@@ -50,6 +52,7 @@ with arcpy.da.SearchCursor(fc, fc_fields) as scursor:
 
 # Remove Nones from a list
 cleaned_list = list(filter(None, filenames))
+
 
 
 def debug_view(**kwargs): # Input variable to view info in script output
@@ -80,6 +83,7 @@ def debug_view(**kwargs): # Input variable to view info in script output
 			return
 
 
+
 # Write information for given variable
 def write_info(name,var): # write_info('var_name',var)
 	write("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
@@ -87,6 +91,8 @@ def write_info(name,var): # write_info('var_name',var)
 	write("   Variable Type: {0}".format(type(var)))
 	write("   Assigned Value: {0}".format(var))
 	write("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+
+
 
 # Time a process
 def runtime(start,finish):
@@ -104,9 +110,12 @@ arcpy.MultipartToSinglepart_management(in_class, out_class)
 finish = dt.now().time()
 
 
+
 def get_count(fc_layer):
     results = int(ap.GetCount_management(fc_layer).getOutput(0))
     return results
+
+
 
 def update_row_tuple(urow, index, val):
 	# For short tuples, slicing and concatenation is faster.
@@ -114,6 +123,8 @@ def update_row_tuple(urow, index, val):
 	edit_row = list(urow)
 	edit_row[index] = val
 	return tuple(edit_row)
+
+
 
 def find_dupes(fc, check_field, update_field):
     with arcpy.da.SearchCursor(fc, [check_field]) as scursor:
@@ -126,3 +137,232 @@ def find_dupes(fc, check_field, update_field):
             else:
                 urow[1] = 'N'
             ucursor.updateRow(urow)
+
+
+
+def copy_fc(source, target, *args, **kwargs): #(s_f)ields, (s_q)uery, (t_f)ields, (t_q)uery
+	# copy_fc(source_var, target_var, s_f=s_fields, s_q=s_query, t_f=t_fields, t_q=t_query)
+    s_fields = kwargs.get('s_f', "'*'") # Default s_fields variable will be '*' for all fields
+    s_query = kwargs.get('s_q', "''")
+    t_fields = kwargs.get('t_f', "'*'") # Default t_fields variable will be '*' for all fields
+    t_query = kwargs.get('t_q', "''")
+
+	with arcpy.da.SearchCursor(source, s_fields, s_query) as scursor:
+		with arcpy.da.InsertCursor(target, t_fields, t_query) as icursor:
+			for row in scursor:
+				icursor.insertRow(row)
+
+
+
+# Conversion toolset
+@gptooldoc('LoadData_production', None)
+def LoadData(in_cross_reference=None, in_sources=None, in_target=None, in_dataset_map_defs=None, row_level_errors=None):
+    """LoadData_production(in_cross_reference, in_sources;in_sources..., in_target, {in_dataset_map_defs;in_dataset_map_defs...}, {row_level_errors})
+
+        Moves features from one schema to another by loading data from a
+        source to a target workspace.  Data mapping rules described in a
+        cross-reference database are applied during the load. All Esri Mapping
+        and Charting solutions products install a cross-reference database
+        that you can use. You can create a cross-reference database using the
+        Create Cross-reference tool.Data that matches the schema defined in
+        the cross-reference database
+        for the source is appended to the target workspace. The cross-
+        reference database contains a DatasetMapping table that lists pairs of
+        source and target dataset names. Each source and target name pair can
+        have a WHERE clause and a subtype. The WHERE clause defines a subset
+        of features in the source to append to the target. Subtype identifies
+        a subtype in the target feature class into which features are loaded.
+
+     INPUTS:
+      in_cross_reference (Workspace):
+          The path to a cross-reference database. Cross-reference databases for
+          each product specification reside in the <install location>\\SolutionNa
+          me\\Desktop10.2\\[ProductName]\\[SpecificationName]\\DataConversion
+          directory.
+      in_sources (Workspace):
+          A list of workspaces that contain the source features to load into the
+          target workspace.
+      in_target (Workspace):
+          The target workspace that contains the schema referenced in the cross-
+          reference database. Source features are loaded into this workspace.
+      in_dataset_map_defs {String}:
+          The source to target feature class mapping list. The format of this
+          string is id | SourceDataset | TargetDataset | WhereClause | Subtype.
+      row_level_errors {Boolean}:
+          Indicates if the tool will log errors that occur while inserting new
+          rows into feature classes and tables in the in_target parameter.
+
+          * ROW_LEVEL_ERROR_LOGGING-Log errors that occur during individual row-
+          level inserts. This is the default.
+
+          * NO_ROW_LEVEL_ERROR_LOGGING-Do not log errors that occur during
+          individual row-level inserts."""
+    from arcpy.geoprocessing._base import gp, gp_fixargs
+    from arcpy.arcobjects.arcobjectconversion import convertArcObjectToPythonObject
+    try:
+        retval = convertArcObjectToPythonObject(gp.LoadData_production(*gp_fixargs((in_cross_reference, in_sources, in_target, in_dataset_map_defs, row_level_errors), True)))
+        return retval
+    except Exception as e:
+        raise e
+
+
+
+import arcpy
+fc_list = [a list of FCs that you want to merge together]
+output_fc = r"C:\temp\test.gdb\merge"
+for fc in fc_list:
+	write("Appending {0} to output feature class".format(fc))
+	if fc_list.index(fc) == 0:
+		arcpy.CopyFeatures_management(fc, output_fc)
+		icursor = arcpy.da.InsertCursor(output_fc, ["SHAPE@","*"])
+	else:
+		with arcpy.da.SearchCursor(fc, ["SHAPE@","*"]) as scursor:
+			for srow in scursor:
+				icursor.insertRow(srow)
+del icursor
+
+
+
+# Gets messages from the ArcGIS tools ran and sends messages to dialog
+def writeresults():
+    messages = GetMessages(0)
+    warnings = GetMessages(1)
+    errors = GetMessages(2)
+    AddMessage(messages)
+    if len(warnings) > 0:
+        AddWarning(warnings)
+    if len(errors) > 0:
+        AddError(errors)
+    return
+
+
+
+'''
+Demonstrates a step progressor by looping through records
+on a table. Use a table with 10,000 or so rows - smaller tables
+just whiz by.
+   1 = table name
+   2 = field on the table
+'''
+import arcpy, time, math
+try:
+    inTable = arcpy.GetParameterAsText(0)
+    inField = 'OID@'
+
+    # Determine number of records in table
+    #
+    record_count = int(arcpy.GetCount_management(inTable).getOutput(0))
+    if record_count == 0:
+        raise ValueError("{0} has no records to count".format(inTable))
+
+    arcpy.AddMessage("Number of rows = {0}\n".format(record_count))
+
+    # Method 1: Calculate and use a suitable base 10 increment
+    # ===================================
+
+    p = int(math.log10(record_count))
+    if not p:
+        p = 1
+    increment = int(math.pow(10, p - 1))
+
+    arcpy.SetProgressor(
+        "step", "Incrementing by {0} on {1}".format(increment, inTable),
+        0, record_count, increment)
+
+    beginTime = time.clock()
+    with arcpy.da.SearchCursor(inTable, [inField]) as cursor:
+        for i, row in enumerate(cursor, 0):
+            if (i % increment) == 0:
+                arcpy.SetProgressorPosition(i)
+            fieldValue = row[0]
+
+    arcpy.SetProgressorPosition(i)
+    arcpy.AddMessage("Method 1")
+    arcpy.AddMessage("\tIncrement = {0}".format(increment))
+    arcpy.AddMessage("\tElapsed time: {0}\n".format(time.clock() - beginTime))
+
+    # Method 2: let's just move in 10 percent increments
+    # ===================================
+    increment = int(record_count / 10.0)
+    arcpy.SetProgressor(
+        "step", "Incrementing by {0} on {1}".format(increment, inTable),
+        0, record_count, increment)
+
+    beginTime = time.clock()
+    with arcpy.da.SearchCursor(inTable, [inField]) as cursor:
+        for i, row in enumerate(cursor, 0):
+            if (i % increment) == 0:
+                arcpy.SetProgressorPosition(i)
+            fieldValue = row[0]
+
+    arcpy.SetProgressorPosition(i)
+    arcpy.AddMessage("Method 2")
+    arcpy.AddMessage("\tIncrement = {0}".format(increment))
+    arcpy.AddMessage("\tElapsed time: {0}\n".format(time.clock() - beginTime))
+
+    # Method 3: use increment of 1
+    # ===================================
+    increment = 1
+    arcpy.SetProgressor("step",
+                        "Incrementing by 1 on {0}".format(inTable),
+                        0, record_count, increment)
+
+    beginTime = time.clock()
+    with arcpy.da.SearchCursor(inTable, [inField]) as cursor:
+        for row in cursor:
+            arcpy.SetProgressorPosition()
+            fieldValue = row[0]
+
+    arcpy.SetProgressorPosition(record_count)
+    arcpy.ResetProgressor()
+    arcpy.AddMessage("Method 3")
+    arcpy.AddMessage("\tIncrement = {0}".format(increment))
+    arcpy.AddMessage("\tElapsed time: {0}\n".format(time.clock() - beginTime))
+
+    arcpy.AddMessage("Pausing for a moment to allow viewing...")
+    time.sleep(2.0)  # Allow viewing of the finished progressor
+
+except Exception as e:
+    arcpy.AddError(e[0])
+
+
+
+#                           __                    __
+#           __       __     \_\  __          __   \_\  __   __       __
+#           \_\     /_/        \/_/         /_/      \/_/   \_\     /_/
+#         .-.  \.-./  .-.   .-./  .-.   .-./  .-.   .-\   .-.  \.-./  .-.
+#        //-\\_//-\\_//-\\_//-\\_//-\\_//-\\_// \\_//-\\_//-\\_//-\\_//-\\
+#      __(   '-'   '-'\  '-'   '-'  /'-'   '-'\__'-'   '-'__/'-'   '-'\__
+#     /_/))            \__       __/\          \_\       /_/           \_\
+#  ___\_//              \_\     /_/  \__
+# /_/  ((                             \_\
+#       )) __
+# __   // /_/
+# \_\_((_/___
+#      ))  \_\
+#      \\
+#       )) _
+# __   // /_/
+# \_\_((
+#      \\
+#       )) _
+# __   // /_/
+# \_\_((_/___
+#      ))  \_\
+#      \\
+#       )) _
+# __   // /_/
+# \_\_((
+#      \\
+#       )) _
+# __   // /_/
+# \_\_((_/___
+#      ))  \_\                __                    __
+#      \\     __       __     \_\  __          __   \_\  __   __       __
+#   __  ))    \_\     /_/        \/_/         /_/      \/_/   \_\     /_/
+#   \_\_((   .-.  \.-./  .-.   .-./  .-.   .-./  .-.   .-\   .-.  \.-./  .-.
+#        \\_//-\\_//-\\_//-\\_//-\\_//-\\_//-\\_// \\_//-\\_//-\\_//-\\_//-\\
+#         '-'\__'-'   '-'\  '-'   '-'  /'-'   '-'\__'-'   '-'__/'-'   '-'\__
+#             \_\         \__       __/\          \_\       /_/           \_\
+#                          \_\     /_/  \__
+#                                        \_\
